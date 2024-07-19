7b7a5a0758b24cc78f4e970c803b95466bd02dcd
Hexadecimal233 <me@onlyra1n.top>
2024-07-19 23:21:38 +0800

    Add Base Layer

---
+++ components/BaseLayer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  layerName: string;
+}>();
+</script>
+
+<template>
+  <div class="layer">
+    <div class="layer-name">{{ layerName }}</div>
+    <div v-motion-slide-visible-once-right class="layer-content">
+      <slot />
+    </div>
+  </div>
+</template>
