412624be0f4b195d934be2a4692a11eeaf04f208
Hexadecimal233 <me@onlyra1n.top>
2024-07-28 09:44:32 +0800

    donations

--- components/QR.vue
+++ components/QR.vue
@@ -8,6 +8,7 @@ defineProps<{
 
 <template>
   <div class="qr-container" :style="{ '--the-qr-color': color }">
+    <div class="qr-bg"></div>
     <div class="qr-wrapper">
       <img class="qr-img" :src="imgUrl" />
       <span class="qr-text">{{ name }}</span>
@@ -17,31 +18,43 @@ defineProps<{
 
 <style>
 .qr-container {
+  position: relative;
   width: 15rem;
-  height: 12rem;
+  height: 16rem;
+  margin: 1rem;
+  /* TODO: Add a color variable */
+}
+
+.qr-bg {
+  position: absolute;
+  top: 4rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
   background-color: var(--the-qr-color);
   border-radius: 0.5rem;
-  position: relative;
   box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.5);
-  /* TODO: Add a color variable */
 }
 
 .qr-wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: absolute;
-  top: -4rem;
+  
+  margin-inline: 1.5rem;
+  
+  * {
+    z-index: 2;
+  }
 }
 
 .qr-img {
   box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.5);
-  width: 75%;
   border-radius: 0.5rem;
 }
 
 .qr-text {
-  margin-top: 1.25rem;
+  margin-top: 1rem;
   color: white;
   font-weight: 600;
   font-size: large;
